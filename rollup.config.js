621481d61f86a997bc99597232c10e13a7a33088
bring-shrubbery <antoni.silvestrovic@gmail.com>
2021-03-20 11:27:34 +0200

    Updated core package

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,60 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-import autoPreprocess from "svelte-preprocess";
-import typescript from "@rollup/plugin-typescript";
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('yarn', ['start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
 export default {
-	input: "src/main.ts",
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
-		format: "iife",
-		name: "app",
-		file: "docs/build/bundle.js"
+		format: 'iife',
+		name: 'app',
+		file: 'docs/build/bundle.js'
 	},
 	plugins: [
 		svelte({
-			preprocess: autoPreprocess(),
-			// enable run-time checks when not in production
-			dev: !production,
-			// we"ll extract any component CSS out into
-			// a separate file - better for performance
-			css: (css) => {
-				css.write("docs/build/bundle.css");
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
 			}
 		}),
-		typescript({ sourceMap: !production }),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
-		// npm, you"ll most likely need these plugins. In
-		// some cases you"ll need additional configuration -
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ["svelte"],
+			dedupe: ['svelte']
 		}),
 		commonjs(),
 
@@ -46,30 +64,13 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload("docs"),
+		!production && livereload('public'),
 
-		// If we"re building for production (npm run build
+		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser(),
+		production && terser()
 	],
 	watch: {
 		clearScreen: false
 	}
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-					stdio: ["ignore", "inherit", "inherit"],
-					shell: true,
-				});
-			}
-		},
-	};
-}
