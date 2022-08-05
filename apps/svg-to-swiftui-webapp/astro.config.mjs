974731618748cc54f5895844cd6021794c24754d
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2022-08-05 08:55:13 +0300

    Fixed base url issue on the website

--- apps/svg-to-swiftui-webapp/astro.config.mjs
+++ apps/svg-to-swiftui-webapp/astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  site: "https://quassum.github.io/",
+  base: "/SVG-to-SwiftUI",
+  integrations: [tailwind(), svelte()],
 });
