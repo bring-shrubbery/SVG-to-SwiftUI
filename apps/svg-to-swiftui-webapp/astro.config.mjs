25f3b3ba01734d1155353d80903c37a4a5c0c263
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2022-12-23 18:28:06 +0200

    Replaced img by astro/image

--- apps/svg-to-swiftui-webapp/astro.config.mjs
+++ apps/svg-to-swiftui-webapp/astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://quassum.github.io/",
 	base: "/SVG-to-SwiftUI",
-	integrations: [tailwind(), svelte()],
+	integrations: [
+		tailwind(),
+		svelte(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 });
