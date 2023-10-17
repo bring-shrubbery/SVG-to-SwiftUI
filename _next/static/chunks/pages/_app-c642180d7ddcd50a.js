1d586d37b8f2fc93af440e49bd98b6ce13014a26
bring-shrubbery <antoni.silvestrovic@gmail.com>
2023-10-17 19:41:08 +0000

    Deploying to gh-pages from @ quassum/SVG-to-SwiftUI@77ee3dbe6914e9d8fdfe4f4da62fe0906fe83fbf 🚀

---
+++ _next/static/chunks/pages/_app-c642180d7ddcd50a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{8744:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(1416)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(8744),__webpack_exec__(4743)}),_N_E=_.O()}]);
---
+++ _next/static/na9d3iUgzTxZeJC9oknTs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-4f197e5ec41478f8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
