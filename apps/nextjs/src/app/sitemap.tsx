cb570076beb50a1ad577bd803ba89a2d0604eccd
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2023-12-17 21:20:04 +0200

    Temporary disabled robots.txt file

--- apps/nextjs/src/app/sitemap.tsx
+++ apps/nextjs/src/app/sitemap.tsx
@@ -5,19 +5,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://svg-to-swiftui.quassum.com",
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://svg-to-swiftui.quassum.com/privacy-policy",
       lastModified: new Date(),
-      changeFrequency: "yearly",
       priority: 0.8,
     },
     {
       url: "https://svg-to-swiftui.quassum.com/terms-and-conditions",
       lastModified: new Date(),
-      changeFrequency: "yearly",
       priority: 0.5,
     },
   ];
