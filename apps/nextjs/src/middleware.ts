7042b5e07a2770dc52692f0a003499a8ccc3a525
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2023-12-21 10:01:59 +0200

    Enabled middleware for _next/static

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  matcher: "/((?!api|favicon.ico).*)",
 };
